68447348dbfbf9ab466c0caad3d689969492471d
Eugene Garcia <eugene.garcia@remedylogic.com>
2024-06-15 19:53:12 +0400

    readme update

--- build.sh
+++ build.sh
@@ -1,14 +1,3 @@
-# docker build \
-#     -f Dockerfile \
-#     --build-arg USER=${USER} \
-#     --build-arg USER_ID=$(id -u) \
-#     --build-arg GROUP_ID=$(id -g) \
-#     --tag ${USER}_base:latest .
-
 docker build \
     -f Dockerfile \
     -t garsiya_base .
-
-# docker build \
-#     -f Dockerfile_train \
-#     -t garsiya_base .
